92e9ab742f2d84f7fa147e7dc41aa05db7d62259
DissonantAU <33020221+DissonantAU@users.noreply.github.com>
2024-11-27 17:52:05 +1100

    Build Script Rework
    Bump to v0.6.85 for testing build script.
    
    Now uses Tasks for building different versions and copying to PluginBuilds.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
-import org.jetbrains.kotlin.utils.addToStdlib.ifFalse
 
 plugins {
     alias(libs.plugins.kotlin.jvm)
@@ -8,9 +7,17 @@ plugins {
     alias(libs.plugins.kotlin.serialization)
 
     id("java")
+    id("application")
+
     alias(libs.plugins.gmazzo.buildconfig)
     alias(libs.plugins.touchportal.plugin.packager)
 }
+
+/* Version */
+val versionMajor: Int = 0
+val versionMinor: Int = 6
+val versionPatch: Int = 85 //Is padded with 0 to left if needed
+
 
 val pluginFullName: String = "Veadotube Touch Portal Plugin"
 val pluginShortName: String = "Veadotube Plugin"
@@ -21,22 +28,48 @@ group = mainClassPackage
 
 tpPlugin.mainClassSimpleName.set(mainClassSimpleName)
 
-/* Version */
-val versionMajor: Int = 0
-val versionMinor: Int = 6
-val versionPatch: Int = 6 //Is padded with 0 to left if needed
+/* Gradle defined run task */
+application.mainClass = "$mainClassPackage.$mainClassSimpleName"
 
-val isRelease = System.getenv("IS_RELEASE") == "YES"
-val versionSuffix: String = isRelease.ifFalse { "-DEV" }.orEmpty()
+tasks.run<JavaExec> {
+    dependsOn(tasks.packagePlugin)
+
+    args(listOf("start"))
+    workingDir =
+        project.layout.buildDirectory.get().dir("plugin").dir(mainClassSimpleName).asFile
+}
+
+
+val buildsDir: Directory = rootProject.layout.projectDirectory.dir("pluginBuilds")
+println("veadotube PluginBuilds Dir: $buildsDir")
+
+val resourcesRelease: Directory = layout.projectDirectory.dir("src/release/resources")
+val resourcesDebug: Directory = layout.projectDirectory.dir("src/debug/resources")
+val resourcesTrace: Directory = layout.projectDirectory.dir("src/trace/resources")
+
+
+
+
+project.extra["releaseName"] = mainClassSimpleName
+println("Release project: ${project.extra["releaseName"]}")
 
 // Version becomes 1203
 val versionCode: Int = versionMajor * 1000 + versionMinor * 100 + versionPatch
-extra["versionCode"] = versionCode
+project.extra["versionCode"] = versionCode
+println("Version Code: $versionCode")
 
-// Version becomes 1.2.03
-val versionName: String = "$versionMajor.$versionMinor.${versionPatch.toString().padStart(2, '0')}$versionSuffix"
-extra["versionName"] = versionName
-version = versionName
+// Version Base Name becomes 1.2.03
+val versionName =
+    "$versionMajor.$versionMinor.${versionPatch.toString().padStart(2, '0')}"
+project.extra["versionBaseName"] = versionName
+project.extra["versionName"] = versionName
+println("Version: $versionName")
+project.version = versionName
+
+
+var releaseTypeProvider: Provider<String> = provider { "DEV" }
+
+calcVersion()
 
 
 buildConfig {
@@ -44,9 +77,14 @@ buildConfig {
 
     buildConfigField("String", "NAME", "\"$pluginFullName\"")
     buildConfigField("String", "NAME_SHORT", "\"$pluginShortName\"")
-    buildConfigField("String", "VERSION_NAME", "\"$versionName\"")
-    buildConfigField("long", "VERSION_CODE", "$versionCode")
+    buildConfigField("String", "VERSION_BASE_NAME", "\"${project.extra["versionBaseName"]}\"")
+    buildConfigField("String", "VERSION_NAME", provider { "\"${project.extra["versionName"]}\"" })
+    buildConfigField("long", "VERSION_CODE", "${project.extra["versionCode"]}")
+
+    println(buildConfigFields)
 }
+
+
 
 repositories {
     mavenCentral()
@@ -113,22 +151,149 @@ java {
     targetCompatibility = JavaVersion.VERSION_1_8
 }
 
+tasks {
 
-val resourcesRelease: Directory = layout.projectDirectory.dir("src/release/resources")
-val resourcesDebug: Directory = layout.projectDirectory.dir("src/debug/resources")
-
-sourceSets {
-    main {
-        resources {
-            when (isRelease){
-                true -> srcDir(resourcesRelease)
-                false -> srcDir(resourcesDebug)
-            }
-
+    // Calculate Version/Build names, etc.
+    register("calculatePluginVersion") {
+        doFirst {
+            calcVersion()
         }
+
+        doLast {
+            println("Setting Source Set Resources to ${releaseTypeProvider.get()}")
+            sourceSets.main {
+                resources {
+                    when (releaseTypeProvider.get()) {
+                        "RELEASE" -> srcDir(resourcesRelease)
+                        "TRACE" -> srcDir(resourcesTrace)
+                        else -> srcDir(resourcesDebug)
+                    }
+                }
+
+            }
+        }
+    }
+
+
+    // Run Version Calculation during Build, including IDE Import/refresh
+    // targeting BuildConfigTask - might not be needed anymore?
+    //project.tasks.withType(BuildConfigTask::class).forEach {
+    //    println("Add calculatePluginVersion to dependsOn $name > Class ${javaClass.name}")
+    //    it.dependsOn(
+    //        named("calculateVersion")
+    //    )
+    //}
+
+
+    /* Task to build the project, copy to PluginBuilds*/
+
+    register<Copy>("copyToPluginBuilds") {
+        duplicatesStrategy = DuplicatesStrategy.WARN
+
+        mustRunAfter(
+            named("calculatePluginVersion")
+        )
+
+        doFirst {
+            println("Copy to '${buildsDir.dir("${project.extra["releaseName"]}_${project.extra["versionBaseName"]}")}'")
+        }
+
+        dependsOn(packagePlugin)
+        from(packagePlugin)
+        into { buildsDir.dir("${project.extra["releaseName"]}_${project.extra["versionBaseName"]}") }
+        rename { filename ->
+            val newFilename = filename.replace(".tpp", "_${project.extra["versionName"]}.tpp")
+            println("Copy $filename to $newFilename")
+            newFilename
+        }
+
+    }
+
+    /* Meta Build Jobs */
+
+    register("buildCopyDevTraceToPluginBuilds") {
+        group = "build"
+
+        doFirst {
+            println("Set to Dev Trace Build")
+            project.ext["BUILD_TYPE"] = "TRACE"
+        }
+
+        finalizedBy(
+            named("calculatePluginVersion"),
+            named("copyToPluginBuilds"),
+        )
+    }
+
+    register("buildCopyDevToPluginBuilds") {
+        group = "build"
+
+        doFirst {
+            println("Set to Dev Debug Build")
+            project.ext["BUILD_TYPE"] = "DEV"
+        }
+
+        finalizedBy(
+            named("calculatePluginVersion"),
+            named("copyToPluginBuilds"),
+        )
+    }
+
+    register("buildCopyReleaseToPluginBuilds") {
+        group = "build"
+
+        doFirst {
+            println("Set to Release Build")
+            project.ext["BUILD_TYPE"] = "RELEASE"
+        }
+
+        finalizedBy(
+            named("calculatePluginVersion"),
+            named("copyToPluginBuilds"),
+        )
+    }
+
+
+    test {
+        useJUnitPlatform()
     }
 }
 
-tasks.test {
-    useJUnitPlatform()
+
+
+
+fun calcVersion() {
+    println("Release project: ${project.extra["releaseName"]}")
+    // Get Environment Var if it exists
+    val envBuildType: String = try {
+        System.getenv("BUILD_TYPE")
+    } catch (_: Throwable) {
+        ""
+    }
+
+    // Release Type - Variable from Build Tasks > Environment Var > Debug Default
+    val releaseType: String =
+        when {
+            project.extra.has("BUILD_TYPE") -> "${project.extra["BUILD_TYPE"]}"
+            envBuildType.isNotBlank() -> System.getenv("BUILD_TYPE")
+            else -> "DEV"
+        }
+
+    println("Release Type: $releaseType")
+
+    val buildSuffix: String = when (releaseType) {
+        "RELEASE" -> ""
+        "TRACE" -> "-DEV-TRACE"
+        else -> "-DEV"
+    }
+
+    // Version Name becomes 1.2.03 or 1.2.03-DEV etc.
+    val versionNameSuffix = "${project.extra["versionBaseName"]}$buildSuffix"
+
+    project.extra["versionName"] = versionNameSuffix
+    project.version = versionNameSuffix
+    println("Version Full Name: $versionNameSuffix")
+
+    releaseTypeProvider = provider { releaseType }
+
 }
