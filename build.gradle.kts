fcbd3d83f90f27f03999675df33159114f2be7b7
DissonantAU <33020221+DissonantAU@users.noreply.github.com>
2025-05-31 01:53:12 +1000

    Bump Version to 0.7.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ plugins {
 */
 val versionMajor: Int = 0
 val versionMinor: Int = 7
-val versionPatch: Int = 3
+val versionPatch: Int = 4
 
 
 val pluginFullName: String = "Veadotube Touch Portal Plugin"
